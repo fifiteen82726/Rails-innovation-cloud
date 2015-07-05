3863b109f3d18f4cbdaf829a2989880c614cd1b6
fifiteen82726 <fifiteen82726@gmail.com>
2015-07-06 00:51:51 +0800

    twelfth step

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -5,8 +5,27 @@ class SignupsController < ApplicationController
 	end
 
 	def create 
-		@signup = Signup.new (signups_params)
+		@signup = Signup.new(signup_params) 
+		if @signup.save 
+			redirect_to '/thanks' 
+		else 
+			render 'new' 
+		end 
 	end
+
+
+
+
+	private 
+	def signup_params
+		params.require(:signup).permit(:email)
+	end
+
+
+
+
+
+	
 
 end
 
